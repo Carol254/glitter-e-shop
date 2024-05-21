dd837fe5f976225c9f464b32f1ee51f70a35a2b0
Caroline <wanguic49@gmail.com>
2024-05-21 15:22:15 +0300

    few changes made

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,5 @@
-import { AfterViewInit, Component, ElementRef, OnInit, viewChild } from '@angular/core';
-declare var $: any;
+import { IMAGE_CONFIG, NgOptimizedImage } from '@angular/common';
+import { AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -10,25 +10,51 @@ declare var $: any;
 })
 
 
-export class HomeComponent {
+export class HomeComponent implements AfterViewInit {
 
   speed:number = 50;
   initialTxt:number = 0;
   requiredTxt:any = 'Adorn yourself with the brilliance of our unique jewelry pieces. Shop now for unmatched elegance.';
+  observer: IntersectionObserver | undefined;
 
-  // @ViewChild('intro', { static: false }) intro!: ElementRef;
+  @ViewChild('intro', { static: false }) intro!: ElementRef;
   
-  // ngOnInit(): void {
-      
-  // }
+ ngAfterViewInit(): void {
+  this.initIntersectionObserver();
+  }
 
-  // typeWriter(){
-  //   if(this.initialTxt > this.requiredTxt.length ){
-  //     this.intro.nativeElement.innerHTML += this.requiredTxt.charAt(this.initialTxt);
-  //     this.initialTxt ++;
-  //     this.speed;
-  //   }
-  // }
+  ngOnDestroy(): void {
+    this.observer?.disconnect();
+  }
+
+
+  initIntersectionObserver() {
+    this.observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          this.resetTypeWriter();
+          this.typeWriter();
+        }
+      });
+    }, { threshold: 0.1 });//calling resetTypeWriter & typewriter fn when the elememnt is 10% visible
+
+    if (this.intro) {
+      this.observer.observe(this.intro.nativeElement);
+    }
+  }
+
+  resetTypeWriter() {
+    this.intro.nativeElement.innerHTML = '';
+    this.initialTxt = 0;
+  }
+
+  typeWriter(){
+    if(this.initialTxt < this.requiredTxt.length ){
+      this.intro.nativeElement.innerHTML += this.requiredTxt.charAt(this.initialTxt);
+      this.initialTxt ++;
+      setTimeout(() => this.typeWriter(), this.speed);
+    }
+  }
 
 }
  

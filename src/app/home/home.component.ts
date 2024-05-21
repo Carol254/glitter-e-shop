8c51f2e3c715f5dd9d4a0fb3b2cba3e157a02254
Caroline <wanguic49@gmail.com>
2024-05-21 10:48:38 +0300

    few changes made

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, viewChild } from '@angular/core';
 declare var $: any;
 
 @Component({
@@ -11,5 +11,24 @@ declare var $: any;
 
 
 export class HomeComponent {
+
+  speed:number = 50;
+  initialTxt:number = 0;
+  requiredTxt:any = 'Adorn yourself with the brilliance of our unique jewelry pieces. Shop now for unmatched elegance.';
+
+  // @ViewChild('intro', { static: false }) intro!: ElementRef;
+  
+  // ngOnInit(): void {
+      
+  // }
+
+  // typeWriter(){
+  //   if(this.initialTxt > this.requiredTxt.length ){
+  //     this.intro.nativeElement.innerHTML += this.requiredTxt.charAt(this.initialTxt);
+  //     this.initialTxt ++;
+  //     this.speed;
+  //   }
+  // }
+
 }
  

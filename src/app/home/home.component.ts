9468970719cdb003397b99f15b75b282c87ee435
Caroline <wanguic49@gmail.com>
2024-05-21 16:47:57 +0300

    few changes made

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,10 @@
-import { IMAGE_CONFIG, NgOptimizedImage } from '@angular/common';
+import { IMAGE_CONFIG, NgOptimizedImage,NgFor } from '@angular/common';
 import { AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
@@ -18,6 +18,23 @@ export class HomeComponent implements AfterViewInit {
   observer: IntersectionObserver | undefined;
 
   @ViewChild('intro', { static: false }) intro!: ElementRef;
+
+  currentIndex = 0;
+  images1 = [
+    '/assets/img-2.jpg',
+    '/assets/img-3.jpg',
+    '/assets/img-4.jpg'
+  ];
+  images2 = [
+    '/assets/img-5.jpg',
+    '/assets/img-6.jpg',
+    '/assets/img-7.jpg'
+  ];
+  quotes = [
+    { text: 'You cant cry on a diamonds shoulder, and diamonds wont keep you warm at night, but theyre sure fun when the sun shines.', author: 'Elizabeth Taylor' },
+    { text: 'The finest workers in stone are not copper or steel tools, but the gentle touches of air and water working at their leisure with a liberal allowance of time', author: 'Henry David Thoreau' },
+    { text: 'Big girls need big diamonds.', author: 'Elizabeth Taylor' }
+  ];
   
  ngAfterViewInit(): void {
   this.initIntersectionObserver();
@@ -54,6 +71,14 @@ export class HomeComponent implements AfterViewInit {
       this.initialTxt ++;
       setTimeout(() => this.typeWriter(), this.speed);
     }
+  }
+
+  prevSlide() {
+    this.currentIndex = (this.currentIndex > 0) ? this.currentIndex - 1 : this.quotes.length - 1;
+  }
+
+  nextSlide() {
+    this.currentIndex = (this.currentIndex < this.quotes.length - 1) ? this.currentIndex + 1 : 0;
   }
 
 }

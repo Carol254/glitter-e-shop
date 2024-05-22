6ccb08a4e0e15b739cef3df486cc5bfaf7e0da8f
Caroline <wanguic49@gmail.com>
2024-05-22 10:23:50 +0300

    few changes made

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,11 @@
+import { AboutComponent } from './about/about.component';
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { ShopComponent } from './shop/shop.component';
 
 export const routes: Routes = [
-    {path:'',component:HomeComponent}
+    {path:'',redirectTo:'/home',pathMatch:'full'},
+    {path:'home',component:HomeComponent},
+    {path:'about',component:AboutComponent},
+    {path:'shop',component:ShopComponent}
 ];

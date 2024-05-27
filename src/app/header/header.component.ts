6e7c75ff510a906b3d3b40facc393c6efddb3188
Caroline <wanguic49@gmail.com>
2024-05-27 11:21:42 +0300

    few changes made

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,14 +1,23 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink,NgClass],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
 
-  
+  isNavbarCollapsed = true; 
+
+  toggleNavbar() {
+    this.isNavbarCollapsed = !this.isNavbarCollapsed;
+  }
+
+  closeNavbar() {
+    this.isNavbarCollapsed = true;
+  }
 }
